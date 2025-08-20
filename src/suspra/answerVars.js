b1adfc77cfc6a734a5a19b1ad7e18d372f696789
ianhorchata <ianhorchata@gmail.com>
2025-08-19 20:28:44 -0400

    fixed datat not appearing in text input

--- src/suspra/answerVars.js
+++ src/suspra/answerVars.js
@@ -1,18 +1,17 @@
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
 const answerVars = {
-    MovementViewVars: {
+    MovementViewVars: reactive({
         m1: 100,
-        m3: ref(''),
+        m3: '',
         m4: 250,
-        m5: ref(''),
-        m6: ref(null),
-        m6Other: ref(''),
-        m7: ref('')
-    }
+        m5: '',
+        m6: null,
+        m6Other: '',
+        m7: ''
+    })
 }
 
 export {
     answerVars
 }
-
